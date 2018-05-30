69e535c00b8fe6d95f4c08ba5860b177ff0441df
Unknown <discepless@yandex.ru>
2018-05-30 10:51:20 +0200

    Test gitIgnore

---
+++ Ruthless Merchant/Assets/Movement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float speed;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(speed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, speed * Input.GetAxis("Vertical") * Time.deltaTime);
+	}
+}
