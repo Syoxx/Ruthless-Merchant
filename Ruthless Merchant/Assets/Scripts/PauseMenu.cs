ab2bd8de3ff9340d623148489267f93631560922
Unknown <blackeagle92@gmx.net>
2018-06-07 12:27:06 +0200

    Added Menu Prototypes
    Added Main Menu Scene
    Added Pause Menu to sample Scene
    Added Prefabs for Menu Buttons + Sliders
    Setup Buildsettings for MainMenu to function
    Added MainMenu.cs and edited PauseMenu.cs

--- Ruthless Merchant/Assets/Scripts/PauseMenu.cs
+++ Ruthless Merchant/Assets/Scripts/PauseMenu.cs
@@ -1,7 +1,17 @@
-﻿namespace RuthlessMerchant
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RuthlessMerchant
 {
-    public class PauseMenu : UI
+
+    public class PauseMenu : MonoBehaviour
     {
+
+        public static bool gameIsPaused = false;
+        public GameObject pauseMenuUI;
+
         private GameSettings settings;
         public GameSettings Settings
         {
@@ -13,6 +23,40 @@
             {
                 settings = value;
             }
+        }
+
+        public void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (gameIsPaused)
+                {
+                    Resume();
+                }
+                else
+                {
+                    Pause();
+                }
+            }
+        }
+
+        public void ReturnToMainMenu()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+
+        public void Resume()
+        {
+            pauseMenuUI.SetActive(false);
+            Time.timeScale = 1f;
+            gameIsPaused = false;
+        }
+
+        void Pause()
+        {
+            pauseMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            gameIsPaused = true;
         }
     }
 }
