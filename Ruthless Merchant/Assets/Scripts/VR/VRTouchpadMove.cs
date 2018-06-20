02d55328a930c9948f27fce2250d93467e7f4abd
Be Lehnert <it.icsti@googlemail.com>
2018-06-20 14:24:10 +0200

    Added simple init menu

---
+++ Ruthless Merchant/Assets/Scripts/VR/VRTouchpadMove.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+
+public class VRTouchpadMove : MonoBehaviour
+{
+    [SerializeField] private Transform rig;
+
+    private Valve.VR.EVRButtonId touchpad = Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad;
+
+    private SteamVR_Controller.Device controller { get {return SteamVR_Controller.Input((int) trackedObj.index);} }
+    private SteamVR_TrackedObject trackedObj;
+
+    // Set axis values to zero
+    private Vector2 axis = Vector2.zero;
+
+    void Start()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    void Update()
+    {
+        if (controller == null)
+        {
+            Debug.Log("Controller not initialized");
+            return;
+        }
+
+        var device = SteamVR_Controller.Input((int) trackedObj.index);
+
+        if (controller.GetTouch(touchpad))
+        {
+            axis = device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0);
+
+            if (rig != null)
+            {
+                rig.position += (transform.right * axis.x + transform.forward * axis.y) * Time.deltaTime;
+                rig.position = new Vector3(rig.position.x, 0, rig.position.z);
+            }
+        }
+    }
+}
