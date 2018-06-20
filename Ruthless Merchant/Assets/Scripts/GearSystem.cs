885203e7247ebdbdfa8f2d53c9b4dacfb9238308
Ricduro <richruk@gmail.com>
2018-06-20 16:59:24 +0200

    Gear changes

--- Ruthless Merchant/Assets/Scripts/GearSystem.cs
+++ Ruthless Merchant/Assets/Scripts/GearSystem.cs
@@ -4,6 +4,7 @@
     {
         private GearSlot[] gearSlots;
         private StatValue[] totalGearStats;
+        private GearSlot ProtectiveGear, Weaponry;
 
         public GearSlot[] GearSlots
         {
@@ -29,8 +30,39 @@
             }
         }
 
+        public GearSystem(bool isPlayer)
+        {           
+            if (!isPlayer)
+            {
+                ProtectiveGear.GearSlotType = GearSlotType.ArmorSlot;
+                Weaponry.GearSlotType = GearSlotType.WeaponSlot;
+                GearSlot[] gearSlots = new GearSlot[] { ProtectiveGear, Weaponry };
+            }
+        }
+
+        public void EquipGear(Item GearItem)
+        {
+            if (GearItem.Type == ItemType.Weapon)
+            {
+                // Remove reference to gear in GearSlot
+                if (ProtectiveGear.Gear != null)
+                {
+                    ProtectiveGear.Gear = null;
+                }
+
+                // Add GearItem
+                ProtectiveGear.Gear = GearItem;
+            }
+            else if (GearItem.Type == ItemType.Gear)
+            {
+
+            }
+            // UpdateGearStats
+        }
+
         public void UpdateGearStats()
         {
+            //TODO: GearStats
             throw new System.NotImplementedException();
         }
     }
