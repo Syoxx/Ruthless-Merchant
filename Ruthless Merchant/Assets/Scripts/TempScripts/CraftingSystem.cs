22fa36f5cb33f76d31efb75010d9b89b82f719a5
Benedikt Zoeller <sephirot5@web.de>
2018-06-20 11:33:56 +0200

    Added Recipes

---
+++ Ruthless Merchant/Assets/Scripts/TempScripts/CraftingSystem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RuthlessMerchant
+{
+    public class CraftingSystem : MonoBehaviour
+    {
+
+        Recipes recipes;
+
+        private void Awake()
+        {
+            recipes = GetComponent<Recipes>();
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Ruthless Merchant/Assets/Scripts/TempScripts/Recipes.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RuthlessMerchant
+{
+    public class Recipes : MonoBehaviour
+    {
+
+        [SerializeField]
+        List<Recipe> recipes;
+
+        [System.Serializable]
+        struct Recipe
+        {
+            [SerializeField]
+            Item Result;
+
+            [SerializeField]
+            List<Materials> materials;
+
+            [SerializeField]
+            bool autoUnlocked;
+
+            [System.Serializable]
+            struct Materials
+            {
+                [SerializeField]
+                Item item;
+
+                [SerializeField]
+                int count;
+            }
+        }
+
+
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
