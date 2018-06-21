823661bfd477f859ba3a39f0524df3d58a91f77c
Warcorbie <k.allgeyer@yahoo.de>
2018-06-21 12:07:52 +0200

    #AssetsUpload
    #SetStationPoint
    #LittleIsland
    #TerrainSizer
    #TestScene

---
+++ Ruthless Merchant/Assets/Scripts/TempScripts/TerrainSizer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainSizer : MonoBehaviour {
+
+	public float sizeX, sizeY, sizeZ;
+	private Vector3 terrainSize;
+	private Terrain terrain;
+
+	void Start () {
+		terrain = GetComponent<Terrain>();
+		terrainSize = new Vector3(sizeX, sizeY, sizeZ);
+		terrain.terrainData.size = terrainSize;
+		
+	}
+	
+
+}
