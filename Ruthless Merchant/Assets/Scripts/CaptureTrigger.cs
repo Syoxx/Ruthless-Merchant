ce00d04c1975f906954e682a6217909967bd2cb2
crone66 <marcel.croonenbroeck@gmail.com>
2018-06-28 17:28:22 +0200

    Added capturing calculations

--- Ruthless Merchant/Assets/Scripts/CaptureTrigger.cs
+++ Ruthless Merchant/Assets/Scripts/CaptureTrigger.cs
@@ -10,9 +10,50 @@ namespace RuthlessMerchant
 {
     public class CaptureTrigger : MonoBehaviour
     {
-        private Faction owner;
+        [SerializeField]
+        private Faction owner = Faction.None;
+        private int captureValue = 0;
+
         private Hero hero;
         private Dictionary<Faction, int> capturingUnits;
+
+        [SerializeField]
+        private CaptureTrigger[] outpostsToFreidenker;
+
+        [SerializeField]
+        private CaptureTrigger[] outpostsToImperialist;
+
+        public int CaptureValue
+        {
+            get
+            {
+                return captureValue;
+            }
+            set
+            {
+                captureValue = value;
+                if (captureValue < -100)
+                    captureValue = -100;
+                else if (captureValue > 100)
+                    captureValue = 100;
+            }
+        }
+
+        public CaptureTrigger[] OutpostsToFreidenker
+        {
+            get
+            {
+                return outpostsToFreidenker;
+            }
+        }
+
+        public CaptureTrigger[] OutpostsToImperialist
+        {
+            get
+            {
+                return outpostsToImperialist;
+            }
+        }
 
         /// <summary>
         /// Get or set the hero of the capture point and sets the owners faction to the faction of the hero
@@ -58,23 +99,39 @@ namespace RuthlessMerchant
         void Start()
         {
             capturingUnits = new Dictionary<Faction, int>();
+            if (owner == Faction.Freidenker)
+                captureValue = -100;
+            else if (owner == Faction.Imperialisten)
+                captureValue = 100;
         }
 
         // Update is called once per frame
         void Update()
         {
-            if(hero != null)
+            if (hero == null && capturingUnits != null)
             {
-                //Capture
-                Debug.Log("Capturing");
-                foreach (KeyValuePair<Faction, int> item in capturingUnits)
+                int freidenkerCount = capturingUnits.ContainsKey(Faction.Freidenker) ? capturingUnits[Faction.Freidenker] : 0;
+                int imperialistenCount = capturingUnits.ContainsKey(Faction.Imperialisten) ? capturingUnits[Faction.Imperialisten] : 0;
+                int monsterCount = capturingUnits.ContainsKey(Faction.Monster) ? capturingUnits[Faction.Monster] : 0;
+
+                if (freidenkerCount > imperialistenCount)
                 {
-                    Debug.Log(item.Key.ToString() + ": " + item.Value.ToString());
+                    if (CaptureValue > 0 || freidenkerCount > imperialistenCount + monsterCount)
+                        CaptureValue--;
+
                 }
-            }
-            else
-            {
-                Debug.Log("Hero is still alive!");
+                else if (imperialistenCount > freidenkerCount)
+                {
+                    if (CaptureValue < 0 || imperialistenCount > freidenkerCount + monsterCount)
+                        CaptureValue++;
+                }
+
+                if (captureValue < -25)
+                    Owner = Faction.Freidenker;
+                else if (captureValue > 25)
+                    Owner = Faction.Imperialisten;
+                else
+                    Owner = Faction.Neutral;
             }
         }
 
