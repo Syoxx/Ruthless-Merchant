50c668a8e2047ecd79e894c07b1671cb3f8553f0
Alberto L <alberto1llv@gmail.com>
2018-06-28 11:14:18 +0200

    Added event system and haptic feedback tests

--- Ruthless Merchant/Assets/Scripts/VR/VRControllerInputDebug.cs
+++ Ruthless Merchant/Assets/Scripts/VR/VRControllerInputDebug.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR;
@@ -15,13 +16,26 @@ namespace RuthlessMerchant
 
         private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
         private SteamVR_TrackedObject trackedObj;
+        private SteamVR_TrackedController trackedCon;
 
-        // Set axis values to zero
-        private Vector2 axis = Vector2.zero;
+        private Vector2 prevTriggerPressed;
+
+        void Awake()
+        {
+            trackedObj = GetComponent<SteamVR_TrackedObject>();
+            trackedCon = GetComponent<SteamVR_TrackedController>();
+        }
 
         void Start()
         {
-            trackedObj = GetComponent<SteamVR_TrackedObject>();
+            prevTriggerPressed = Vector2.zero;
+
+            trackedCon.TriggerClicked += OnTriggerClicked;
+            trackedCon.PadTouched += OnTouchpadTouched;
+            trackedCon.PadClicked += OnTouchpadClicked;
+            trackedCon.MenuButtonClicked += OnMenuButtonClicked;
+            trackedCon.Gripped += OnGripped;
+            trackedCon.SteamClicked += OnSteamClicked;
         }
 
         void Update()
@@ -35,39 +49,96 @@ namespace RuthlessMerchant
             string debugMessage = "";
             var device = SteamVR_Controller.Input((int)trackedObj.index);
 
-            if (controller.GetTouch(touchpad))
-            {
-                axis = device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0);
-                debugMessage += "Touchpad Touch Input! X: " + axis.x + ", Y: " + axis.y + "\n";
-            }
+            //if (controller.GetTouch(touchpad))
+            //{
+            //    axis = device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0);
+            //    debugMessage += "Touchpad Touch Input! X: " + axis.x + ", Y: " + axis.y + "\n";
+            //}
 
-            if (controller.GetPressDown(touchpad))
-            {
-                axis = device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0);
-                debugMessage += "Touchpad Click Input! X: " + axis.x + ", Y: " + axis.y + "\n";
-            }
+            //if (controller.GetPressDown(touchpad))
+            //{
+            //    axis = device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0);
+            //    debugMessage += "Touchpad Click Input! X: " + axis.x + ", Y: " + axis.y + "\n";
+            //}
 
             Vector2 triggerPressed = device.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger);
 
-            debugMessage += "Hairtrigger Pressed: " + triggerPressed + "\n";
-
-            if (triggerPressed.x > 0.96f)
+            if (triggerPressed.x != prevTriggerPressed.x)
             {
-                debugMessage += "Hairtrigger Click Input!" + "\n";
+                debugMessage += "Hairtrigger Pressed: " + triggerPressed + "\n";
             }
 
-            if (controller.GetPressDown(appMenuButton))
-            {
-                debugMessage += "Application-Menu Button Input!" + "\n";
-                controller.TriggerHapticPulse(200);
-            }
+            prevTriggerPressed = triggerPressed;
 
-            if (controller.GetPressDown(gripButton))
-            {
-                debugMessage += "Grip Button Input!" + "\n";
-            }
+            //if (triggerPressed.x > 0.96f)
+            //{
+            //    debugMessage += "Hairtrigger Click Input!" + "\n";
+            //}
+
+            //if (controller.GetPressDown(appMenuButton))
+            //{
+            //    debugMessage += "Application-Menu Button Input!" + "\n";
+            //    TriggerPulse((ushort)10000);
+            //}
+
+            //if (controller.GetPressDown(gripButton))
+            //{
+            //    debugMessage += "Grip Button Input!" + "\n";
+            //}
 
             Debug.Log(debugMessage);
+        }
+
+        void OnTriggerClicked(object sender, ClickedEventArgs e)
+        {
+            Debug.Log("Trigger clicked");
+            TriggerPulse(1000000);
+        }
+
+        void OnTouchpadTouched(object sender, ClickedEventArgs e)
+        {
+            Debug.Log("Touchpad touched");
+        }
+
+        void OnTouchpadClicked(object sender, ClickedEventArgs e)
+        {
+            Debug.Log("Touchpad clicked");
+        }
+
+        void OnMenuButtonClicked(object sender, ClickedEventArgs e)
+        {
+            Debug.Log("MenuButton Clicked");
+        }
+
+        void OnGripped(object sender, ClickedEventArgs e)
+        {
+            Debug.Log("Gripped");
+        }
+
+        void OnSteamClicked(object sender, ClickedEventArgs e)
+        {
+            Debug.Log("Steam Clicked");
+        }
+
+        IEnumerator TriggerPulse(int microseconds)
+        {
+            ushort rest = 0;
+
+            if(microseconds > 3500)
+            {
+                rest = (ushort)(microseconds - 3500);
+                microseconds = 3500;
+            }
+
+            controller.TriggerHapticPulse((ushort)microseconds);
+
+            if (rest > 0)
+            {
+                yield return new WaitForSeconds(0.0035f);
+                StartCoroutine(TriggerPulse(rest));
+            }
+
+            yield return null;
         }
     }
 }
