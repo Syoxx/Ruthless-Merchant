ef0d0eafa774a01d257ba691357bb75067678506
Be Lehnert <it.icsti@googlemail.com>
2018-06-28 16:06:11 +0200

    Added Link

--- Ruthless Merchant/Assets/Scripts/VR/VRObjectInteraction.cs
+++ Ruthless Merchant/Assets/Scripts/VR/VRObjectInteraction.cs
@@ -1,6 +1,12 @@
-﻿using System.Collections;
+﻿/*
+ * https://www.youtube.com/watch?v=CfAl7vqJV70
+ */
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
 namespace RuthlessMerchant
 {
@@ -13,6 +19,9 @@ namespace RuthlessMerchant
         private SteamVR_TrackedObject trackedObj;
 
         private Valve.VR.VREvent_HapticVibration_t hapticVibration;
+        private Valve.VR.EVRInputError triggerHaptic;
+
+        private GameObject pickup;
         
         // Use this for initialization
         void Start()
@@ -29,24 +38,30 @@ namespace RuthlessMerchant
                 return;
             }
 
-            if (controller.GetPressDown(gripButton))
+            if (controller.GetPressDown(gripButton) && pickup != null)
             {
+                pickup.transform.parent = this.transform;
+                pickup.GetComponent<Rigidbody>().useGravity = false;
                 Debug.Log("Grip Button was just pressed.");
             }
 
-            if (controller.GetPressUp(gripButton))
+            if (controller.GetPressUp(gripButton) &&  pickup != null)
             {
+                pickup.transform.parent = null;
+                pickup.GetComponent<Rigidbody>().useGravity = true;
                 Debug.Log("Grip Button was just unpressed.");
             }
         }
 
         private void OnTriggerEnter(Collider collider)
         {
+            pickup = collider.gameObject;
             Debug.Log("Trigger entered");
         }
 
         private void OnTriggerExit(Collider collider)
         {
+            pickup = null;
             Debug.Log("Trigger exited.");
         }
     }
