2fad89e9334b51643282e225c30fe1541e553693
Nikolas Pietrek <npietrek13@googlemail.com>
2018-07-04 11:33:14 +0200

    added Script to Unload/Load all NPCs

---
+++ Ruthless Merchant/Assets/Scripts/TempScripts/DisableGameObjects.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableGameObjects : MonoBehaviour {
+
+    GameObject[] npcArray;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            npcArray = GameObject.FindGameObjectsWithTag("NPC");
+            Debug.Log("disabling all NPCs");
+            foreach (GameObject go in npcArray)
+            {
+                go.SetActive(false);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            Debug.Log("enabling all NPCs");
+            foreach (GameObject go in npcArray)
+            {
+                go.SetActive(true);
+            }
+        }
+	}
+}
