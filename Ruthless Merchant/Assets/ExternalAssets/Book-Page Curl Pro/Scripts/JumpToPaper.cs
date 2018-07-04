073b21703cf0008969405be985885a421cc7e60a
SovietAra <discepless@yandex.ru>
2018-07-04 09:16:21 +0200

    - Added tag Manager "Book_Inventory"
    - Adjusted Scene to last changes
    - Adjusted Script "Jump to Paper"
    
    An Marcel. Script ist "JumpToPaper.cs". Befindet sich unter "ExternalAssets" -> "Book-Page.." -> "Scripts"

--- Ruthless Merchant/Assets/ExternalAssets/Book-Page Curl Pro/Scripts/JumpToPaper.cs
+++ Ruthless Merchant/Assets/ExternalAssets/Book-Page Curl Pro/Scripts/JumpToPaper.cs
@@ -34,6 +34,17 @@ public class JumpToPaper : MonoBehaviour
         }
     }
 
+	//Count the Childs in PNL_ZoneForItem (Located in Page 1 and Page 2 as a child). The counter == The items in _pagesList 
+	//
+	//
+	//This shit is broken.
+	//
+	//An Marcel - Hier wird der falscher "itemsAmout" ausgespückt. Somit kann leider die Funktion "pageForCurrentWeaponPlacement" nicht funktionieren. 
+	//Die Aufgabe ist - die Items von der pagesList zu zählen. Diese werden als "PNL_ItemZone" unter "Page1/2" ->  "PNL_ZoneForItem" als prefab hergestellt (Beim aufnahme eines Items)
+	//Wenn du Fragen bezüglich den Items hast, sollst du an Richard wenden.
+	//Falls du eine bessere Idee hast, kannst du es auch gerne deine Lösung schreiben.
+	//P.S. Dieser Code funktioniert auch, wenn das Buch Deactivated in der Scene ist ABER es muss in Inspector aktiviert sein. Später werde ich die bestimmte Funktionen einfach an bestimmte Stellen aufrufen um das zu vermeiden.
+	//Also, im Prinzip, du kannst das Buch wieder zuschließen um Items aufzuheben ("J") aber wie gesagt, bei "default" lass es an (Du startest mit geöffnetes Buch)
     private int CountWeaponsInInventory()
     {
         int itemsAmount = 0;
@@ -51,10 +62,13 @@ public class JumpToPaper : MonoBehaviour
         Debug.Log("Items" + itemsAmount);
         return itemsAmount;
     }
+
+	// Here we decide for the Page where should be the weapon placed.
+	// CountWeaponsInInvetory = current itemsAmount, _maxWeaponsPerPage (adjusted at Player prefab). -1 stands for a case n/m where n=m.
     public int pageForCurrentWeaponPlacement()
     {
       // Debug.Log("Alo" + Mathf.Floor((CountWeaponsInInventory() - 2) / GameObject.Find("NewPlayerPrefab").GetComponent<Player>()._maxWeaponsPerPage));
-        return (int) Mathf.Floor((CountWeaponsInInventory()  - 2)/ GameObject.Find("NewPlayerPrefab").GetComponent<Player>()._maxWeaponsPerPage);
+        return (int) Mathf.Floor((CountWeaponsInInventory()  - 1)/ GameObject.Find("NewPlayerPrefab").GetComponent<Player>()._maxWeaponsPerPage);
     }
 
   
@@ -62,7 +76,7 @@ public class JumpToPaper : MonoBehaviour
 	public void SwitchToMenu ()
 	{
         
-		Debug.Log ("I'm here");
+		//Debug.Log ("I'm here");
 	    _myBook.CurrentPaper = 2;
 
         
