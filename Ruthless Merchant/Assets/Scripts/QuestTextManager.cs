f8c51a63087d9f84002f94f4c483a4c32f68bd5f
Daniil Masliy <discepless@yandex.ru>
2018-07-09 14:02:31 +0200

    Quest Counter (INT) will be updated

---
+++ Ruthless Merchant/Assets/Scripts/QuestTextManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace RuthlessMerchant
+{
+    public class QuestTextManager : MonoBehaviour
+    {
+        private string questDialogue;
+        [SerializeField][Tooltip("Drag a wood Prefab there")]
+        private Item woodPrefab;
+        [SerializeField] [Tooltip("Drag a iron Prefab there")]
+        private Item ironPrefab;
+
+        private int woodCounter;
+        private int ironCounter;
+        Player player;
+        private TextMeshProUGUI textMesh;
+        void Start()
+        {
+            textMesh = GetComponent<TextMeshProUGUI>();
+
+            player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            woodCounter = player.Inventory.GetNumberOfItems(woodPrefab);
+            ironCounter = player.Inventory.GetNumberOfItems(ironPrefab);
+
+            questDialogue = "1.    Waffe aufsammeln \n" +
+               "2.    Den Wald besuchen\n" +
+               "3.    Holz sammeln (" + woodCounter + "/5)\n" +
+               "4.    Iron sammeln (" + ironCounter + "/5)\n" +
+               "5.    Alte Waffe zerlegen\n" +
+               "6.    Crafting aus Eisen + Holz = neue Waffe\n" +
+               "7.    Mit einem Händler handeln";
+
+            textMesh.text = questDialogue.ToString();
+        }
+    }
+}
