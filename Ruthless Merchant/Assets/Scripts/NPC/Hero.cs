fe304a6c282b123635573b5817b18fd237f82280
crone66 <marcel.croonenbroeck@gmail.com>
2018-07-25 13:27:08 +0200

    Added wander and patrol actions to hero

--- Ruthless Merchant/Assets/Scripts/NPC/Hero.cs
+++ Ruthless Merchant/Assets/Scripts/NPC/Hero.cs
@@ -3,13 +3,16 @@
 //
 //---------------------------------------------------------------
 
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace RuthlessMerchant
 {
     public class Hero : Warrior
     {
-        private QuestItem Quest;
+        private QuestItem quest;
+
+        public CaptureTrigger Outpost;
 
         [Header("Patrol settings")]
         [SerializeField]
@@ -24,14 +27,34 @@ namespace RuthlessMerchant
 
         public override void Start()
         {
+            if (patrolActive)
+            {
+                patrolActive = false;
+                if (PossiblePatrolPaths != null && PossiblePatrolPaths.Length > 0)
+                    PatrolPoints = new List<Waypoint>(GetRandomPath(PossiblePatrolPaths, false, 3)).ToArray();
+
+                SetCurrentAction(new ActionPatrol(ActionNPC.ActionPriority.Low), null);
+            }
             base.Start();
         }
 
         public override void Update()
         {
+            if(Outpost != null)
+            {
+                if (Outpost.IsHeroAway && quest.Equals(default(QuestItem)) && (Waypoints.Count == 0 || waypoints[0].Transform != Outpost.transform))
+                {
+                    AddNewWaypoint(new Waypoint(Outpost.transform, true, 0), true);
+                    SetCurrentAction(new ActionMove(ActionNPC.ActionPriority.Medium), null, true);
+                }
+                else if (!Outpost.IsHeroAway)
+                {
+                    if(CurrentAction is ActionIdle || CurrentAction == null)
+                        SetCurrentAction(new ActionWander(), null);
+                }
+            }
+
             base.Update();
         }
-
-
     }
 }
