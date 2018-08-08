d0d84b73ee94294e769c545e799b728dce8e4edb
crone66 <marcel.croonenbroeck@gmail.com>
2018-08-08 21:38:56 +0200

    fixed small wander distance

--- Ruthless Merchant/Assets/Scripts/NPC/Actions/ActionWander.cs
+++ Ruthless Merchant/Assets/Scripts/NPC/Actions/ActionWander.cs
@@ -28,7 +28,8 @@ namespace RuthlessMerchant
                 NavMeshPath path = new NavMeshPath();
                 if (agent.CalculatePath(wanderTarget, path) && path.status == UnityEngine.AI.NavMeshPathStatus.PathComplete)
                 {
-                    parent.AddNewWaypoint(new Waypoint(parent.transform.position + new Vector3(Random.Range(-2.0f, 2.0f), 0, Random.Range(-2.0f, 2.0f)), true, Random.Range(1, 5)), true);
+                    
+                    parent.AddNewWaypoint(new Waypoint(parent.transform.position + new Vector3(Random.Range(1.1f, 4.0f) * (Random.Range(0, 2) - 1), 0, Random.Range(1.1f, 4.0f) * (Random.Range(0, 2) - 1)), true, Random.Range(1, 5)), true);
                     break;
                 }
             }
