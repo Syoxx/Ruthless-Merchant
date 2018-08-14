b55f159f3b2f3636a828d2f5eacf59b36c06e068
Nikolas Pietrek <npietrek13@googlemail.com>
2018-08-14 13:12:11 +0200

    updated Prefabs, added failsafe for Fading, added Tag

--- Ruthless Merchant/Assets/Scripts/Respawn/respawnLogic.cs
+++ Ruthless Merchant/Assets/Scripts/Respawn/respawnLogic.cs
@@ -28,16 +28,16 @@ namespace RuthlessMerchant
         private float shortestDistance;
         private GameObject nearestRespawnPoint;
         private GameObject respawnPoint;
-        private GameObject FadeCanvas;
         private bool fadingDone;
         private Vector3 respawnPosition;
 
         /// <summary>
-        /// gets the FadeCanvas at the Start
+        /// gets the Fade Image at the Start
         /// </summary>
         private void Start()
         {
-            FadeCanvas = GameObject.FindGameObjectWithTag("FadeCanvas");
+            if (fadeImage == null)
+                fadeImage = GameObject.FindGameObjectWithTag("FadeImage").GetComponent<Image>();
         }
 
         /// <summary>
