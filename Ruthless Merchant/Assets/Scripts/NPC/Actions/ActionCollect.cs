0f64cbe6ef9ad2d422e94460c6a23b80a790e6db
crone66 <marcel.croonenbroeck@gmail.com>
2018-08-14 15:48:39 +0200

    Fixed collect action

--- Ruthless Merchant/Assets/Scripts/NPC/Actions/ActionCollect.cs
+++ Ruthless Merchant/Assets/Scripts/NPC/Actions/ActionCollect.cs
@@ -12,27 +12,32 @@ namespace RuthlessMerchant
 
         public override void StartAction(NPC parent, GameObject other)
         {
-            base.StartAction(parent, other);
+            parent.AddNewWaypoint(new Waypoint(other.transform.position, true, 3.0f), true);
+
+            base.StartAction(parent, null);
+            this.other = other;      
         }
 
         public override void Update(float deltaTime)
         {
             if (!agent.pathPending && agent.remainingDistance < agent.baseOffset)
             {
-                //Collect
-                Debug.Log("other: " + other);
-                goal.CollectableFound(other.GetComponent<Material>());
-                GameObject.Destroy(other);
+                if (other != null)
+                {
+                    //Collect
+                    goal.CollectableFound(other.GetComponent<Material>());
+                    UnityEngine.GameObject.DestroyImmediate(other);
+
+                    //Set new goal and Action
+                    parent.SetCurrentAction(new ActionIdle(), null, true, true);
+                    goal.CalcNextWaypoint();
+                }
             }
             base.Update(deltaTime);
         }
 
         public override void EndAction(bool executeEnd = true)
         {
-            if(executeEnd)
-            {
-                goal.CalcNextWaypoint();
-            }
             base.EndAction(executeEnd);
         }
     }
