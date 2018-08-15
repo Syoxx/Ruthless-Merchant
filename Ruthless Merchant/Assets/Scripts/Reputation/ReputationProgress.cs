dc890991762ecb48882b85b52cfa407b930c4d4d
Nikolas Pietrek <npietrek13@googlemail.com>
2018-08-15 16:07:01 +0200

    saving work

--- Ruthless Merchant/Assets/Scripts/Reputation/ReputationProgress.cs
+++ Ruthless Merchant/Assets/Scripts/Reputation/ReputationProgress.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class ReputationProgress : MonoBehaviour {
 
+    private float currentRepImperialists, currentRepFreethinkers;
+    private bool maxRepImperialists, maxRepFreethinkers;
+    private GameObject checkMark, questName, questText;
 	// Use this for initialization
 	void Start () {
-		
+        checkMark = this.gameObject.transform.GetChild(1).GetChild(0).GetChild(0).gameObject;
+        questName = this.gameObject.transform.GetChild(0).gameObject;
+        questText = this.gameObject.transform.GetChild(3).gameObject;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        currentRepImperialists = GameObject.FindGameObjectWithTag("Player").GetComponent<Reputation>().ImperalistenStanding;
 	}
 }
