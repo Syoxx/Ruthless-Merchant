de5b68205fa6cd42f29e9bb9a0dc471318b27aeb
Nikolas Pietrek <npietrek13@googlemail.com>
2018-08-16 16:50:57 +0200

    small fix

--- Ruthless Merchant/Assets/Scripts/NPC/NPC.cs
+++ Ruthless Merchant/Assets/Scripts/NPC/NPC.cs
@@ -207,10 +207,10 @@ namespace RuthlessMerchant
             }
         }
 
-        public override void DestroyInteractivObject()
+        public override void DestroyInteractiveObject(float delay = 0)
         {
             NPCCount[faction]--;
-            base.DestroyInteractivObject();
+            base.DestroyInteractiveObject(delay);
         }
 
         private void CheckReactionState()
