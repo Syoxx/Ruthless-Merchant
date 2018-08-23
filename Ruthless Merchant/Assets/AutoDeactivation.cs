02fae493f13ceeb499087410a953b09d320ace66
Benedikt Zoeller <sephirot5@web.de>
2018-08-23 15:31:21 +0200

    Tutorial-Text now deactivates automatically

---
+++ Ruthless Merchant/Assets/AutoDeactivation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AutoDeactivation : MonoBehaviour {
+
+    [SerializeField]
+    TextMeshProUGUI text;
+
+    [SerializeField]
+    float timePerLetter;
+
+    float time;
+
+    int textLength;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(time + textLength * timePerLetter - Time.time);
+		if(Time.time > time + textLength * timePerLetter)
+        {
+            gameObject.SetActive(false);
+        }
+	}
+
+    private void OnEnable()
+    {
+        time = Time.time;
+        textLength = text.text.Length;
+    }
+}
