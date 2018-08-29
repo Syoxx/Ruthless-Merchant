bb933a9c16c64ff84e1713482d2fe2f6758f6798
crone66 <marcel.croonenbroeck@gmail.com>
2018-08-29 17:43:31 +0200

    Fixed a bug where objects who exited the hear area where added instead of removed from the heared gameobject list

--- Ruthless Merchant/Assets/Scripts/NPC/HearScript.cs
+++ Ruthless Merchant/Assets/Scripts/NPC/HearScript.cs
@@ -28,7 +28,7 @@ namespace RuthlessMerchant
         {
             if (!other.isTrigger && other.transform != transform.parent && (other.CompareTag("NPC") || other.CompareTag("Player") || other.CompareTag("Item")))
             {
-                npc.OnEnterHearArea(other);
+                npc.OnExitHearArea(other);
             }
         }
     }
