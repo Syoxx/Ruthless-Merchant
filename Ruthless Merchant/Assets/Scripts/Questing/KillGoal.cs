1f9cbaa96728abb265656220d1473b84bbd7d201
Ricduro <richruk@gmail.com>
2018-08-31 16:50:39 +0200

    KillingGoals class

--- Ruthless Merchant/Assets/Scripts/Questing/KillGoal.cs
+++ Ruthless Merchant/Assets/Scripts/Questing/KillGoal.cs
@@ -2,37 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KillGoal : Goal {
+namespace RuthlessMerchant {
+    public class KillGoal : Goal {
 
-    [SerializeField]
-    private int EnemyID;
+        //[SerializeField]
+        //private int EnemyID;
+        [SerializeField]
 
-    public KillGoal(int enemyID, string description, bool completed, int currentAmount, int requiredAmount, List<Transform> waypoints)
-    {
-        this.EnemyID = enemyID;
-        this.Description = description;
-        this.Completed = completed;
-        this.CurrentAmount = currentAmount;
-        this.RequiredAmount = requiredAmount;
+
+        
+        QuestButton questButton;
+
+        public KillGoal(/*int enemyID, */string description, bool completed, int currentAmount, int requiredAmount, List<Transform> waypoints)
+        {
+            //this.EnemyID = enemyID;
+            this.Description = description;
+            this.Completed = completed;
+            this.CurrentAmount = currentAmount;
+            this.RequiredAmount = requiredAmount;
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+        }
+
+        private void Update()
+        {
+            //fot Testing
+            if (Input.GetKeyDown(KeyCode.K))
+                EnemyDied();
+        }
+
+        void EnemyDied()
+        {
+            CurrentAmount++;
+            Evaluate();
+        }
     }
 
-    public override void Initialize()
-    {
-        base.Initialize();
-
-    }
-
-    private void Update()
-    {
-        //fot Testing
-        if (Input.GetKeyDown(KeyCode.K))
-            EnemyDied();
-    }
-
-    void EnemyDied()
-    {
-        CurrentAmount++;
-        Evaluate();
-    }
 }
-
---
+++ Ruthless Merchant/Assets/Scripts/Questing/KillingGoals.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RuthlessMerchant
+{
+    public class KillingGoals : MonoBehaviour
+    {
+
+        private bool questingEnabled;
+        private KillGoal killGoal;
+        Hero hero;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void AssignQuest()
+        {
+            if (killGoal != null && questingEnabled)
+            {
+                Debug.Log("kill quest assigned.");
+
+                GameObject target = null; // targeted by the hero
+                float distance = float.MaxValue;
+
+                // detect monsters
+                GameObject[] Monsters = GameObject.FindGameObjectsWithTag("NPC");
+                
+                for (int i = 0; i < Monsters.Length; i++)
+                {
+                    if (Monsters[i].gameObject.name == "NPC_Monster(Clone)")
+                    {
+                        float newDistance = Vector3.Distance(gameObject.transform.position, Monsters[i].transform.position);
+
+                        if (newDistance < distance)
+                        {
+                            target = Monsters[i];
+                        }
+                    }
+                }
+
+                // killgoal.setAction
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                questingEnabled = true;
+            }
+        }
+    }
+}
