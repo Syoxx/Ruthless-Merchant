3c83fc5e345fcf690ae2077ba52c00fe4dab5a25
Riku <blackeagle92@gmx.net>
2018-08-31 16:05:23 +0200

    Added AudioEvents.cs for FMOD Testing

---
+++ Ruthless Merchant/Assets/Scripts/AudioScripts/AudioEvents.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Audio
+{
+    class AudioEvents : MonoBehaviour
+    {
+#if FMOD
+
+        void NPC_Step(string _path)
+        {
+            FMODUnity.RuntimeManager.PlayOneShot(_path, this.transform.position);
+            //event:/Characters/Monster/MonsterStep
+        }
+
+#endif
+    }
+}
