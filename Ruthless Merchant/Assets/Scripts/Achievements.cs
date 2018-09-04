1ac51846f9d6ddb9e391f13f7b0384b7d3380f2a
GregorFrankenberg <gregor@vfrankenberg.de>
2018-09-04 10:57:47 +0200

    changed achievement-texts

--- Ruthless Merchant/Assets/Scripts/Achievements.cs
+++ Ruthless Merchant/Assets/Scripts/Achievements.cs
@@ -34,12 +34,12 @@ namespace RuthlessMerchant {
         "Enough fooling around, time to make some money. It looks like there is nothing in my storage, so I should go out there and find materials near the “Ancient Trade Route”. \n" ,
         "This seems to be a very ressource-rich area. I should build a outpost so I can quick travel to this destination via my map\n" ,
         "Now that I gathered everything I need to make new friends and money, I should travel to the capital of the Open-Minded.\n" ,
-        "They say the Open-Minded are a XXXXX culture. Blabla about open minded. I should strengthen this culture and sell them weapons in order to weaken the other clan and raise the need of better weapons.\n" ,
+        "They say the Open-Minded are a strange culture. They live in little wooden houses and I don’t think they take hygiene that serious. But they are great in doing ambushes. I should strengthen this culture and sell them weapons in order to weaken the other clan and raise the need of better weapons.\n" ,
         "I just received an order from a hero of the Imperialists. They want me to steal a special weapon at “Eastern Mountain Trail” and deliver it to their capital. It is probably stored in their military tent…\n",
         "Since the way to the Imperialists capital is quite long. I can make some money on my way with selling potions I can craft. Someone once told me many Vipershrooms can be found near “Uphill Path”.\n",
         "Across the bridge near “Green Leaf Path” is a known place for many other ingredients like Creaky Tuber that I could use.\n",
         "Now I can brew some potion and sell these for a good price.\n",
-        "I should arrive at the “Imperialist capital” soon and finally sell this special weapon. Blablabla about Imperialisten.\n",
+        "I should arrive at the “Imperialist capital” soon and finally sell this special weapon. These Imperialists are a strong faction. But they are hiding behind great stone walls. They love all the stuff of the ancient civilization. I should keep this in mind.\n",
         "A rather large disbalance has arisen between both fractions. I should sell lots of good weapons to the Imperalists now, so they can fight back. However, I should always try to keep a balance…\n"
     };
 
