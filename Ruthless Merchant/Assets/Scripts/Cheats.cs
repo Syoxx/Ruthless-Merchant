da9232e5e57f2004e1da90ff84d6eb59f4c16094
crone66 <marcel.croonenbroeck@gmail.com>
2018-09-04 14:53:07 +0200

    Refectoring of the cheats script

--- Ruthless Merchant/Assets/Scripts/Cheats.cs
+++ Ruthless Merchant/Assets/Scripts/Cheats.cs
@@ -1,5 +1,7 @@
-﻿//Author: Marcel Croonenbroeck
-
+﻿//---------------------------------------------------------------
+// Author: Marcel Croonenbroeck
+//
+//---------------------------------------------------------------
 using UnityEngine;
 
 namespace RuthlessMerchant
@@ -15,78 +17,142 @@ namespace RuthlessMerchant
             //Disable Tutorial
             if (Input.GetKey(KeyCode.F1))
             {
-                GameObject player = GameObject.Find("NewPlayerPrefab");
-                Tutorial tut = player.GetComponent<Tutorial>();
-                tut.DisableTutorial();
+                SkipTutorial();
             }
 
             //Teleport to Imperialist city
             if (Input.GetKey(KeyCode.F2))
             {
-                GameObject gobj = GameObject.Find("Main City Imperalists 2");
-                GameObject player = GameObject.Find("NewPlayerPrefab");
-                if (gobj != null && player != null)
-                {
-                    player.transform.position = gobj.transform.position;
-                    Debug.Log("Teleported to imperialist city");
-                }
-
+                TeleportToImperialists();
             }
 
             //Teleport to freemind city
             if (Input.GetKey(KeyCode.F3))
             {
-                GameObject gobj = GameObject.Find("CityFreidenker");
-                GameObject player = GameObject.Find("NewPlayerPrefab");
-                if (gobj != null && player != null)
-                {
-                    player.transform.position = gobj.transform.position;
-                    Debug.Log("Teleported to freeminds city");
-                }
+                TeleportToFreeminds();
             }
 
+            //Imperialist wins
             if (Input.GetKey(KeyCode.F4))
             {
-                GameObject[] gobjs = GameObject.FindGameObjectsWithTag("CaptureTrigger");
-                for (int i = 0; i < gobjs.Length; i++)
+                SetFactionVictory(Faction.Imperialisten);
+            }
+
+            //Freemind wins
+            if (Input.GetKey(KeyCode.F5))
+            {
+                SetFactionVictory(Faction.Freidenker);
+            }
+
+            //Skip collection part of tutorial
+            if(Input.GetKey(KeyCode.F6))
+            {
+                SkipCollectionTutorial();
+            }
+
+            //Skip crafting part of tutorial
+            if(Input.GetKey(KeyCode.F7))
+            {
+                SkipPlayerCaveTutorial();
+            }
+
+            //Don't ask...
+            if(Input.GetKey(KeyCode.F10))
+            {
+                Praise();
+            }
+        }
+
+        /// <summary>
+        /// Disables the tutorial
+        /// </summary>
+        private void SkipTutorial()
+        {
+            GameObject player = GameObject.Find("NewPlayerPrefab");
+            Tutorial tut = player.GetComponent<Tutorial>();
+            tut.DisableTutorial();
+        }
+
+        /// <summary>
+        /// Teleports the player to the main city of the freeminds
+        /// </summary>
+        private void TeleportToFreeminds()
+        {
+            GameObject gobj = GameObject.Find("CityFreidenker");
+            GameObject player = GameObject.Find("NewPlayerPrefab");
+            if (gobj != null && player != null)
+            {
+                player.transform.position = gobj.transform.position;
+                Debug.Log("Teleported to freeminds city");
+            }
+        }
+
+        /// <summary>
+        /// Teleports the player to the main city of the imperialists
+        /// </summary>
+        private void TeleportToImperialists()
+        {
+            GameObject gobj = GameObject.Find("Main City Imperalists 2");
+            GameObject player = GameObject.Find("NewPlayerPrefab");
+            if (gobj != null && player != null)
+            {
+                player.transform.position = gobj.transform.position;
+                Debug.Log("Teleported to imperialist city");
+            }
+
+        }
+
+        /// <summary>
+        /// Skips the collection part of the tutorial
+        /// </summary>
+        private void SkipCollectionTutorial()
+        {
+            GameObject player = GameObject.Find("NewPlayerPrefab");
+            Tutorial tut = player.GetComponent<Tutorial>();
+            tut.CollectionIsCompleted();
+        }
+
+        /// <summary>
+        /// Skips the player cave part of the tutorial
+        /// </summary>
+        private void SkipPlayerCaveTutorial()
+        {
+            GameObject player = GameObject.Find("NewPlayerPrefab");
+            Tutorial tut = player.GetComponent<Tutorial>();
+            tut.WorkbenchIsCompleted();
+            tut.AlchemyIsCompleted();
+            tut.SmithIsCompleted();
+        }
+
+        /// <summary>
+        /// Set the owner of all outposts to a given faction
+        /// </summary>
+        /// <param name="faction">Owner faction</param>
+        private void SetFactionVictory(Faction faction)
+        {
+            GameObject[] gobjs = GameObject.FindGameObjectsWithTag("CaptureTrigger");
+            for (int i = 0; i < gobjs.Length; i++)
+            {
+                CaptureTrigger trigger = gobjs[i].GetComponent<CaptureTrigger>();
+                if (faction == Faction.Freidenker)
                 {
-                    CaptureTrigger trigger = gobjs[i].GetComponent<CaptureTrigger>();
-                    trigger.Owner = Faction.Imperialisten;
+                    trigger.Owner = faction;
+                    trigger.CaptureValue = -100;
+                }
+                else if(faction == Faction.Imperialisten)
+                {
+                    trigger.Owner = faction;
                     trigger.CaptureValue = 100;
                 }
             }
+        }
 
-            if (Input.GetKey(KeyCode.F5))
-            {
-                GameObject[] gobjs = GameObject.FindGameObjectsWithTag("CaptureTrigger");
-                for (int i = 0; i < gobjs.Length; i++)
-                {
-                    CaptureTrigger trigger = gobjs[i].GetComponent<CaptureTrigger>();
-                    trigger.Owner = Faction.Freidenker;
-                    trigger.CaptureValue = -100;
-                }
-            }
-
-            if(Input.GetKey(KeyCode.F6))
-            {
-                GameObject player = GameObject.Find("NewPlayerPrefab");
-                Tutorial tut = player.GetComponent<Tutorial>();
-                tut.CollectionIsCompleted();
-            }
-
-            if(Input.GetKey(KeyCode.F7))
-            {
-                GameObject player = GameObject.Find("NewPlayerPrefab");
-                Tutorial tut = player.GetComponent<Tutorial>();
-                tut.WorkbenchIsCompleted();
-                tut.AlchemyIsCompleted();
-                tut.SmithIsCompleted();
-            }
-
-            if(Input.GetKey(KeyCode.F10))
-            {
-                Debug.Log("Praise Lord Crone!");
-            }
+        /// <summary>
+        /// Don't ask... :D
+        /// </summary>
+        private void Praise()
+        {
+            Debug.Log("Praise Lord Crone!");
         }
     }
 }
