11284257dc3cd2d10a909d48d3eff7d2ff7d78bf
Alberto L <alberto1llv@gmail.com>
2018-09-13 14:09:34 +0200

    Added Loading Scene

---
+++ Ruthless Merchant/Assets/Scripts/VR/VR_LoadingChanger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class VR_LoadingChanger : MonoBehaviour
+{
+    TextMeshPro text;
+    float timer;
+    const float timerLimit = 0.5f;
+
+	void Awake ()
+    {
+        text = GetComponent<TextMeshPro>();
+        timer = Random.value;
+    }
+	
+	void Update ()
+    {
+        timer += Time.deltaTime;
+
+        if(timer > timerLimit)
+        {
+            int pointsInText = text.text.Replace("Loading", "").Length;
+
+            if (pointsInText >= 3)
+                text.text = "Loading";
+
+            else
+                text.text += ".";
+
+            timer = 0;
+        }
+	}
+}
