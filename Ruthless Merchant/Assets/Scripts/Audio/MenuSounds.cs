38c5f50357cf8a15c0fabd6bda3e3f872ca55478
Riku <blackeagle92@gmx.net>
2018-09-13 14:39:51 +0200

    Added Menu Hover Sounds and tweaked NPC Sword sounds

---
+++ Ruthless Merchant/Assets/Scripts/Audio/MenuSounds.cs
@@ -0,0 +1,31 @@
+﻿//---------------------------------------------------------------
+// Author: Fabian Subat
+//
+//---------------------------------------------------------------
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSounds : MonoBehaviour {
+
+    private bool soundPlayed;
+    private FMOD.Studio.EventInstance soundevent;
+    FMOD.Studio.PLAYBACK_STATE playbackState;
+
+    private void OnMouseOver()
+    {
+        if (!soundPlayed)
+        {
+            soundPlayed = true;
+            FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/UI Generic/Mouseover");
+        }
+
+    }
+
+    private void OnMouseExit()
+    {
+        soundPlayed = false;
+    }
+}
