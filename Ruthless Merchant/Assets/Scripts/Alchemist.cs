c207a7b9e0911fc0d1c97d2342dbc413a04ee93d
Benedikt Zoeller <sephirot5@web.de>
2018-09-14 14:32:37 +0200

    added price to potions

--- Ruthless Merchant/Assets/Scripts/Alchemist.cs
+++ Ruthless Merchant/Assets/Scripts/Alchemist.cs
@@ -58,6 +58,38 @@ namespace RuthlessMerchant
         }
 
         /// <summary>
+        /// Creates the Value for the Potion
+        /// </summary>
+        /// <param name="atk">ATK-Parameter of the potion</param>
+        /// <param name="def">DEF-Parameter of the potion</param>
+        /// <param name="speed">Speed-Parameter of the potion</param>
+        /// <param name="hp">Health Regeneration-Parameter of the potion</param>
+        /// <returns>final value as int</returns>
+        int PotionValue(int atk, int def, int speed, int hp)
+        {
+            int value = 3;
+
+            for(int i = atk; i > 0; i--)
+            {
+                value += i;
+            }
+            for (int i = def; i > 0; i--)
+            {
+                value += i;
+            }
+            for (int i = speed; i > 0; i--)
+            {
+                value += i;
+            }
+            for (int i = hp; i > 0; i--)
+            {
+                value += i;
+            }
+
+            return value;
+        }
+
+        /// <summary>
         /// Creates a Name for the Potion
         /// </summary>
         /// <param name="atk">ATK-Parameter of the potion</param>
@@ -235,7 +267,7 @@ namespace RuthlessMerchant
             potion.ItemInfo.ItemRarity = Rarity(atkCount, defCount, speedCount, hpCount);
             potion.ItemInfo.ItemSprite = RandomSprite();
             potion.ItemInfo.MaxStackCount = 1;
-            potion.ItemInfo.ItemValue = 1;
+            potion.ItemInfo.ItemValue = PotionValue(atkCount, defCount, speedCount, hpCount);
 
             //Adds potion to infentory
             Player p = caller.GetComponent<Player>();
